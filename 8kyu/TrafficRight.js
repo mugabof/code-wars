c0b8ae0a75a1fd1ff0f9a81f2105aff9d446aa39
Mugabo <mugaboe50@gmail.com>
2025-05-17 15:41:26 +0200

    my first codewars

---
+++ 8kyu/TrafficRight.js
@@ -0,0 +1,14 @@
+function updateLight(current) {
+  
+    //your code here
+   if (current==='green'){
+     return 'yellow' 
+    }
+    else if(current==='yellow'){
+      return 'red';
+    }
+      
+  else{ 
+    return 'green';
+  }
+  }
