64606ca16def0b830394e9eaf3a9fded11f1c140
Mugabo <mugaboe50@gmail.com>
2025-08-20 10:33:47 +0200

    greetings.

---
+++ 8kyu/greetingTranslator.js
@@ -0,0 +1,12 @@
+function helloWorld(lang) {
+    if (lang == 'fr') {
+        return 'Bonjour tout le monde';
+    } else if (lang == 'es') {
+        return 'Hola, Mundo';
+    } else {
+        return 'Hello, World';
+    }
+}
+console.log(helloWorld('en'));
+console.log(helloWorld('fr'));
+console.log(helloWorld('es'));
