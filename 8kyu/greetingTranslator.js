ec429ff0e50869e122fb1138b331fcdcf5384095
Mugabo <mugaboe50@gmail.com>
2025-08-20 10:42:10 +0200

    Another way using CASE

--- 8kyu/greetingTranslator.js
+++ 8kyu/greetingTranslator.js
@@ -1,3 +1,5 @@
+// Using If Condition
+
 function helloWorld(lang) {
     if (lang == 'fr') {
         return 'Bonjour tout le monde';
@@ -10,3 +12,27 @@ function helloWorld(lang) {
 console.log(helloWorld('en'));
 console.log(helloWorld('fr'));
 console.log(helloWorld('es'));
+console.log('---------------Now lets use Case----------------')
+
+// Use Case function 
+
+function helloWorld(lang) {
+  switch (lang) {
+    case "es": // Spanish
+      return "¡Hola, Mundo!";
+    case "de": // German
+      return "Hallo, Welt!";
+    case "fr": // French
+      return "Bonjour, le monde!";
+    default: // English
+      return "Hello, World!";
+  }
+}
+
+// Test calls
+console.log(helloWorld("en")); // English
+console.log(helloWorld("es")); // Spanish
+console.log(helloWorld("de")); // German
+console.log(helloWorld("fr")); // French
+console.log(helloWorld("jp")); // Not supported -> defaults to English
+
