1731cdd3f2459b74d37f6679edcfb52db2bae537
Mugabo <mugabofrancois1997@gmail.com>
2026-01-27 22:51:43 +0200

    Learining Functions syntax

--- 8kyu/fcccontinue.js
+++ 8kyu/fcccontinue.js
@@ -26,3 +26,43 @@ switch(dayOfWeek){
   default:
   console.log('Excellent we no have day today uhhhhh!!!')
 }
+// Dealing with Functions now 
+
+
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+
+greet("Carter"); // Hello, Alice!
+greet("Alice"); // Hello, Nick!
+// Use of return in Funmction
+
+function calculateSum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(calculateSum(100,234));
+// An anonymous function is a function without a name that can be assigned to a variable like this:
+
+const sum = function (num1, num2) {
+  return num1 + num2;
+};
+
+console.log(sum(3, 4)); // 7
+
+//Functions support default parameters, allowing you to set default values for parameters. These default values are used if the function is called without an argument for that parameter. Here's an example:
+
+function greetings(name = "Mugabo") {
+  console.log("Hello, " + name + "!");
+}
+
+greetings(); // Hello, Guest!
+greetings("Anna"); // Hello, Anna!
+
+//Arrow Functions 
+
+const calculateArea = (width, height) => {
+  return width * height;
+}; 
+
+console.log("to calulate area using Arrow func:" + calculateArea(5, 3)); // 15 and can be put into one line: const calculateArea = (width, height) => width * height;
