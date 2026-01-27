171dd1e832a1bf3fe5d44a1ca902ad1184d7689c
Mugabo <mugabofrancois1997@gmail.com>
2026-01-27 23:22:27 +0200

    Build a Calculator with functions

--- 8kyu/fcccontinue.js
+++ 8kyu/fcccontinue.js
@@ -66,3 +66,48 @@ const calculateArea = (width, height) => {
 }; 
 
 console.log("to calulate area using Arrow func:" + calculateArea(5, 3)); // 15 and can be put into one line: const calculateArea = (width, height) => width * height;
+
+//Build a Calculator with Functions
+function calculateSum(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(calculateSum(2, 5));
+console.log(calculateSum(10, 10));
+console.log(calculateSum(5, 5));
+
+function calculateDifference(num1, num2) {
+  return num1 - num2;
+}
+
+console.log(calculateDifference(22, 5));
+console.log(calculateDifference(12, 1));
+console.log(calculateDifference(17, 9));
+
+function calculateProduct(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(calculateProduct(13, 5));
+
+
+function calculateQuotient(num1, num2) {
+  return num2 === 0 ? "Error: Division by zero" : num1 / num2;
+}
+
+console.log(calculateQuotient(7, 11));
+console.log(calculateQuotient(3, 0));
+
+function calculateSquare(num) {
+  return num ** 2;
+}
+
+console.log(calculateSquare(2));
+console.log(calculateSquare(9));
+
+function calculateSquareRoot(num) {
+  return Math.sqrt(num);
+}
+
+console.log(calculateSquareRoot(25));
+console.log(calculateSquareRoot(100));
