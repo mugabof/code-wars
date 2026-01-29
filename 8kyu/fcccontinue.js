0c4bacd0697ff6868ad42d4e0766011abc091938
Mugabo <mugabofrancois1997@gmail.com>
2026-01-29 23:25:40 +0200

    Truncate a String Algorithm

--- 8kyu/fcccontinue.js
+++ 8kyu/fcccontinue.js
@@ -266,3 +266,21 @@ let result = isLeapYear(year);
 // Output the result to the console
 console.log(result);
 
+
+console.log("***************************NEXT TOPIC*******************************");
+
+//Implement Truncate a string Algorithm
+function truncateString(str, num) {
+  if (str.length <= num) {
+    return str;
+    } 
+    else {
+    return str.slice(0, num) + "...";
+  }
+}
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 8));
+console.log(truncateString("Peter Piper picked a peck of pickled peppers", 11));
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length));
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length + 2));
+console.log(truncateString("A-", 1));
+console.log(truncateString("Absolutely Longer", 2) );
