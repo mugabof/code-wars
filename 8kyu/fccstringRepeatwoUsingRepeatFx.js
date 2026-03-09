9ff2d5c691b2651623f443788f93e1998bfbac67
Mugabo <mugabofrancois1997@gmail.com>
2026-03-09 18:49:12 +0200

    Build missing letter detector

--- 8kyu/fccstringRepeatwoUsingRepeatFx.js
+++ 8kyu/fccstringRepeatwoUsingRepeatFx.js
@@ -20,3 +20,25 @@ console.log(repeatStringNumTimes("hello", 0)); // Should return ""
 console.log(repeatStringNumTimes("world", -2)); // Should return ""
 console.log(repeatStringNumTimes("java", 2)); // Should return "javajava"
 console.log(repeatStringNumTimes("Mugabo carter!", 5)); // Should return "!!!!!"
+
+//=============================================================================================
+function fearNotLetter(str) {
+  // Loop through the string and compare the character codes of consecutive letters
+  for (let i = 0; i < str.length - 1; i++) {
+    // If the difference in character codes is greater than 1, then we have a missing letter
+    if (str.charCodeAt(i) + 1 !== str.charCodeAt(i + 1)) {
+      // Return the missing letter by converting the character code back to a letter
+      return String.fromCharCode(str.charCodeAt(i) + 1);
+    }
+  }
+  
+  // If no letter is missing, return undefined
+  return undefined;
+}
+
+// Example Test Cases
+console.log(fearNotLetter("abce")); // Should return "d"
+console.log(fearNotLetter("abcdef")); // Should return undefined
+console.log(fearNotLetter("abcdefghjklm")); // Should return "i"
+console.log(fearNotLetter("opqrs")); // Should return "t"
+console.log(fearNotLetter("xyz")); // Should return undefined
